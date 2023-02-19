ab65bf3b80cf18f3425ab03c626246ed4c757e3d
FTigashov <tigashov.filipp@yandex.ru>
2023-02-20 01:09:20 +0300

    created my arrayList

--- src/main/java/task2/StartApp.java
+++ src/main/java/task2/StartApp.java
@@ -3,18 +3,30 @@ package task2;
 import java.util.*;
 public class StartApp {
     public static void main(String[] args) {
-        // Добавление, получение, удаление
         MyLinkedList<Integer> list = new MyLinkedList<>();
+        MyArrayList<Integer> array = new MyArrayList<>();
+
         list.addFirst(1);
         list.addFirst(2);
         list.addFirst(3);
         list.addFirst(4);
         list.addLast(5);
 
-        // Тестирование
         System.out.println(list);
         System.out.println(list.get(1));
         list.remove(1);
         System.out.println(list);
+
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+
+        System.out.println(array);
+
+        System.out.println(array.get(0));
+        array.remove(1);
+        System.out.println(array);
     }
 }
