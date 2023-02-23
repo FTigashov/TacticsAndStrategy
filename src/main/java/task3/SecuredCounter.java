0124149969f55df0d52c5c8734640cac6a18c211
FTigashov <tigashov.filipp@yandex.ru>
2023-02-23 14:08:20 +0300

    change structure with increase counter

--- src/main/java/task3/SecuredCounter.java
+++ src/main/java/task3/SecuredCounter.java
@@ -3,24 +3,30 @@ package task3;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Counter {
-    private volatile int counter = 0;
-    private final Lock lock = new ReentrantLock();
+public class SecuredCounter {
 
     public static void main(String[] args) {
-        Counter count = new Counter();
-        System.out.printf("Начальное значение счетчика: %d%n", new Counter().counter);
+        System.out.printf("Начальное значение счетчика: %d%n", new Counter().getCounter());
+        Counter counter = new Counter();
 
-        Thread t1 = new Thread(count::increaseCounter);
-        Thread t2 = new Thread(count::increaseCounter);
-        Thread t3 = new Thread(count::increaseCounter);
+        Thread t1 = new Thread(counter);
+        Thread t2 = new Thread(counter);
+        Thread t3 = new Thread(counter);
 
         t1.start();
         t2.start();
         t3.start();
     }
 
-    private void increaseCounter() {
+
+}
+
+class Counter implements Runnable {
+    private volatile int counter = 0;
+    private final Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
         for (int i = 1; i <= 10; i++) {
             try {
                 lock.lock();
@@ -31,6 +37,10 @@ public class Counter {
                 lock.unlock();
             }
         }
+    }
 
+    public int getCounter() {
+        return counter;
     }
 }
+
