a6cb3133cf3611acddb90dcdce9466c6a2f548ff
FTigashov <tigashov.filipp@yandex.ru>
2023-03-21 00:46:06 +0300

    created db config files

---
+++ src/main/resources/database_config/db_script.sql
@@ -0,0 +1,9 @@
+-- Создание базы данных для работы с hibernate
+CREATE DATABASE hibernate_db;
+
+-- Создание таблицы Student
+CREATE TABLE Student (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    mark BIGINT
+)
